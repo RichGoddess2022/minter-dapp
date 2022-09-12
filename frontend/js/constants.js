daf20bf2d90b201b6a8f6bca6b45d3b9a73b8289
Rich Goddess <hello@richgoddess.io>
2022-09-12 12:24:40 +0800

    updated colors

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,12 +1,12 @@
 const contractAddress = "0x936447B2ee4225e146fe8129EC72F6dE24cf12b1"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to Rich Goddess Paradise.";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Rich Goddess Paradise is a collection of 111 AI-generated goddesses radiating their love and light on the Ethereum Blockchain.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
-const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Public Minting Open!!";
+const h1_presale_mint = "Pre-Sale Minting Open!";
+const h1_public_mint = "Public Minting Open!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
 const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
